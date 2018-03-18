60b3f6767975add73030976f72409ce011783621
PeterEpsteen <petereps1@gmail.com>
2018-03-18 15:33:28 -0700

    Filter brew list and ordering

--- server/controllers/brewController.js
+++ server/controllers/brewController.js
@@ -1,7 +1,8 @@
 
 const db = require('../config/database');
 
-
+const filter_methods = ["Chemex", "Aeropress", "Pour Over", "French Press",
+"Espresso", "Moka Pot", "Other"];
 
 //--------------Brew----------------------/
 function getBrew(req, res, next) {
@@ -28,7 +29,12 @@ function getBrews(req, res, next) {
     }
     req.query.offset = size * (pageNo - 1);
     req.query.limit = (size > 0) ? size : 50;
-    db.any('(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id) OFFSET ${offset} LIMIT ${limit}', req.query)
+
+    var filter = (req.query.hasOwnProperty("filter_method")) ? "WHERE b.brew_method = '"+req.query.filter_method+"'" : "";
+    req.query.filter_method = filter;
+    let sort = (req.query.hasOwnProperty("sort")) ? req.query.sortBy : "points";
+    req.query.sort = sort;
+    db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id ${filter_method}) ORDER BY b.${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
         .then(data => {
             res.status(200).json({
                 status:'success',
@@ -36,7 +42,7 @@ function getBrews(req, res, next) {
                 message: 'Connection working'
             });
         })
-        .catch(err => {return next(err)});
+        .catch(err => {return next(err)});      
 }
 function addBrew(req, res, next) {
     db.none('INSERT INTO brew (user_id, brew_name, brew_date, '+
