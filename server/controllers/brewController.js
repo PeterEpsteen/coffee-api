31c9d0fdea5978a5863921e24c864f7f40f3016f
PeterEpsteen <petereps1@gmail.com>
2018-03-18 16:33:13 -0700

    Error

--- server/controllers/brewController.js
+++ server/controllers/brewController.js
@@ -34,9 +34,9 @@ function getBrews(req, res, next) {
     req.query.filter_method = filter;
     let sort = (req.query.hasOwnProperty("sort")) ? req.query.sort : "points";
     req.query.sort = sort;
-    var query = pgp.as.format("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query);
+    var query = pgp.as.format("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort^} DESC OFFSET ${offset} LIMIT ${limit}", req.query);
     console.log(query);
-    db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
+    db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort^} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
         .then(data => {
             res.status(200).json({
                 status:'success',
