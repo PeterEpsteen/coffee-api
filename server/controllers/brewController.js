a7af079d394de9a782338274ba36abaf8c16bbca
PeterEpsteen <petereps1@gmail.com>
2018-03-18 16:22:59 -0700

    Server

--- server/controllers/brewController.js
+++ server/controllers/brewController.js
@@ -30,13 +30,14 @@ function getBrews(req, res, next) {
     req.query.offset = size * (pageNo - 1);
     req.query.limit = (size > 0) ? size : 50;
 
-    var filter = (req.query.hasOwnProperty("filter_method")) ? "WHERE b.brew_method = '"+req.query.filter_method+"'" : "";
+    var filter = (req.query.hasOwnProperty("filter_method")) ? req.query.filter_method : "%";
     req.query.filter_method = filter;
-    let sort = (req.query.hasOwnProperty("sort")) ? "b." + req.query.sort : "b.points";
+    let sort = (req.query.hasOwnProperty("sort")) ? req.query.sort : "points";
     req.query.sort = sort;
-    db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id)  OFFSET ${offset} LIMIT ${limit}", req.query)
+    var query = pgp.as.format("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query);
+    console.log(query);
+    db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
         .then(data => {
-            console.log(data);
             res.status(200).json({
                 status:'success',
                 data: data,
@@ -45,7 +46,7 @@ function getBrews(req, res, next) {
         })
         .catch(err => {return next(err)});      
 
-        // db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
+        // db.any("(SELECT b.*, u.username FROM (SELECT b.*, COUNT(c.brew_id) AS comments FROM brew AS b LEFT JOIN brew_comment AS c ON c.brew_id = b.brew_id GROUP BY b.brew_id) AS b INNER JOIN users as u ON b.user_id = u.id WHERE b.brew_method LIKE ${filter_method}) ORDER BY ${sort} DESC OFFSET ${offset} LIMIT ${limit}", req.query)
 }
 function addBrew(req, res, next) {
     db.none('INSERT INTO brew (user_id, brew_name, brew_date, '+
